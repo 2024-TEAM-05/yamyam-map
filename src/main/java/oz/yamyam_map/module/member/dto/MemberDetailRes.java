92a6f3aa9465895ce611b14c334650c5c5028aea
toughCircle <toughCircle@gmail.com>
2024-08-30 08:13:43 +0900

    feat: 성공 응답 객체 생성

---
+++ src/main/java/oz/yamyam_map/module/member/dto/MemberDetailRes.java
@@ -0,0 +1,29 @@
+package oz.yamyam_map.module.member.dto;
+
+import lombok.Builder;
+import lombok.Value;
+import oz.yamyam_map.module.member.entity.Member;
+
+@Value
+@Builder
+public class MemberDetailRes {
+
+	Long id;
+	String account;
+
+	Double latitude;
+	Double longitude;
+	Boolean receiveRecommendations;
+
+	public static MemberDetailRes fromEntity(Member member) {
+
+		return MemberDetailRes.builder()
+			.id(member.getId())
+			.account(member.getAccount())
+			.latitude(member.getLatitude())
+			.longitude(member.getLongitude())
+			.receiveRecommendations(member.getReceiveRecommendations())
+			.build();
+
+	}
+}
