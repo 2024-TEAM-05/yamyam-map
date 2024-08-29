4d5731800b86c31bd1af98b832a451ec1d9349e1
toughCircle <toughCircle@gmail.com>
2024-08-30 08:20:42 +0900

    feat: controller 계층 구현

--- src/main/java/oz/yamyam_map/module/member/controller/MemberController.java
+++ src/main/java/oz/yamyam_map/module/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package oz.yamyam_map.module.member.controller;
 
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.*;
 import static oz.yamyam_map.common.code.StatusCode.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import oz.yamyam_map.common.BaseApiResponse;
-import oz.yamyam_map.module.member.dto.MemberSignupReq;
+import oz.yamyam_map.module.member.dto.request.MemberSignupReq;
+import oz.yamyam_map.module.member.dto.response.MemberDetailRes;
 import oz.yamyam_map.module.member.service.MemberService;
 
 @RestController
@@ -27,6 +31,15 @@ public class MemberController implements MemberControllerDocs {
 	public BaseApiResponse<Void> signUp(@RequestBody @Valid MemberSignupReq request) {
 		memberService.signUp(request);
 		return BaseApiResponse.of(SIGN_UP_ACCEPTED);
+	}
+
+	@GetMapping("/detail")
+	@ResponseStatus(OK)
+	public BaseApiResponse<MemberDetailRes> getMemberDetail(@RequestHeader("Authorization") String token) {
+
+		MemberDetailRes memberDetail = memberService.getMemberDetail(token.substring(7));
+
+		return BaseApiResponse.of(OK, memberDetail);
 	}
 
 }
