adfab4d3c70a73760251d5588c463127e6a3021d
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-29 12:36:07 +0900

    feat: 맛집 엔티티 추가 (#6)

--- src/main/java/oz/yamyam_map/module/restaurant/entity/Restaurant.java
+++ src/main/java/oz/yamyam_map/module/restaurant/entity/Restaurant.java
@@ -1,6 +1,11 @@
 package oz.yamyam_map.module.restaurant.entity;
 
+import java.awt.*;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import oz.yamyam_map.common.enums.RestaurantBusinessType;
 
 @Entity
 @Getter
@@ -20,4 +26,21 @@ public class Restaurant {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private RestaurantBusinessType businessType;
+
+	private String phoneNumber;
+
+	@Column(nullable = false, columnDefinition = "GEOMETRY")
+	private Point location;
+
+	private String NumberAddress;
+
+	private String RoadNameAddress;
+
 }
