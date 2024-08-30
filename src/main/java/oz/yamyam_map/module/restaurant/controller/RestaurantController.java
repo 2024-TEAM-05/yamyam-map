a3e543f3e6a6fbe8ff981173a94d03166e646e71
jooda00 <dragon2000a@gmail.com>
2024-08-30 17:26:44 +0900

    rename: RestaurantService service 패키지로 이동 (#9)

--- src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
+++ src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import oz.yamyam_map.common.BaseApiResponse;
 import oz.yamyam_map.common.code.StatusCode;
-import oz.yamyam_map.module.restaurant.RestaurantService;
 import oz.yamyam_map.module.restaurant.dto.request.ReviewUploadReq;
 import oz.yamyam_map.module.restaurant.dto.response.RestaurantDetailRes;
+import oz.yamyam_map.module.restaurant.service.RestaurantService;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,6 +42,6 @@ public class RestaurantController implements RestaurantControllerDocs {
 	public BaseApiResponse<RestaurantDetailRes> getRestaurantDetails(
 		@PathVariable(name = "restaurantId") Long restaurantId) {
 		RestaurantDetailRes restaurantDetailRes = restaurantService.getRestaurantDetails(restaurantId);
-		return BaseApiResponse.of(OK, StatusCode.OK.getMessage(), restaurantDetailRes);
+		return BaseApiResponse.of(StatusCode.OK, restaurantDetailRes);
 	}
 }
