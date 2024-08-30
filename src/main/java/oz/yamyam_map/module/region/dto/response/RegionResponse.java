e0fdf1d22b2ef7a6fbd20d31ab2ea71e80953013
Doeun <id5eun30@gmail.com>
2024-08-30 18:43:35 +0900

    feat: 지역별 맛집목록 조회 고려하여 DTO 수정 (#27)

--- src/main/java/oz/yamyam_map/module/region/dto/response/RegionResponse.java
+++ src/main/java/oz/yamyam_map/module/region/dto/response/RegionResponse.java
@@ -3,6 +3,8 @@ package oz.yamyam_map.module.region.dto.response;
 import java.util.List;
 import java.util.Map;
 
+import org.locationtech.jts.geom.Point;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,13 +14,19 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class RegionResponse {
 
-	@Schema(description = "시/도 리스트")
-	private List<String> provinces;
-
-	@Schema(description = "각 시/도에 해당하는 시군구 리스트")
+	@Schema(description = "시/도 별로 그룹화 한 시군구 리스트")
 	private Map<String, List<String>> cityDistricts;
 
-	public static RegionResponse of(List<String> provinces, Map<String, List<String>> cityDistricts ) {
-		return new RegionResponse(provinces, cityDistricts);
+	public static RegionResponse of(Map<String, List<String>> cityDistricts) {
+		return new RegionResponse(cityDistricts);
+	}
+
+	// 시군구 정보 같이 뿌려주기 위한 CityDistrict InnerClass
+	@Getter
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
+	public static class CityDistrict {
+		private Long id;
+		private String name;
+		private Point location;
 	}
 }
