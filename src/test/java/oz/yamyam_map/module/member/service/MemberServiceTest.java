82a9aa2813a4d71c86e457abfb33014c3d8a3dd0
toughCircle <toughCircle@gmail.com>
2024-08-30 12:24:18 +0900

    fix: 서비스 레이어의 JwtManager 의존성 제거, 컨트롤러에서 관련 작업 후 조회한 값을 서비스 레이어로 전달

--- src/test/java/oz/yamyam_map/module/member/service/MemberServiceTest.java
+++ src/test/java/oz/yamyam_map/module/member/service/MemberServiceTest.java
@@ -1,8 +1,8 @@
 package oz.yamyam_map.module.member.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static oz.yamyam_map.common.code.StatusCode.*;
 
 import java.util.Optional;
 
@@ -15,21 +15,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import oz.yamyam_map.exception.custom.DataNotFoundException;
-import oz.yamyam_map.module.auth.jwt.JwtManager;
 import oz.yamyam_map.module.member.dto.response.MemberDetailRes;
 import oz.yamyam_map.module.member.entity.Member;
 import oz.yamyam_map.module.member.repository.MemberRepository;
 
 @ExtendWith(MockitoExtension.class)
-class MemberServiceTest {
+public class MemberServiceTest {
 
 	private final PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
 
 	@Mock
 	private MemberRepository memberRepository;
-
-	@Mock
-	private JwtManager jwtManager;
 
 	@InjectMocks
 	private MemberService memberService;
@@ -38,36 +34,34 @@ class MemberServiceTest {
 	@DisplayName("유효한 토큰으로 회원 정보를 정상적으로 가져오는 경우")
 	void getMemberDetailSuccess() {
 		// Given
-		String token = "valid_token";
 		Long memberId = 1L;
+		Member mockMember = Member.signUp("account", "password11!", passwordEncoder); // Mock Member 객체 생성
 
-		Member member = Member.signUp("account", "password11!", passwordEncoder);
-		when(jwtManager.getMemberId(token)).thenReturn(memberId);
-		when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+		when(memberRepository.findById(memberId)).thenReturn(Optional.of(mockMember));
 
 		// When
-		MemberDetailRes result = memberService.getMemberDetail(token);
+		MemberDetailRes result = memberService.getMemberDetail(memberId);
 
 		// Then
 		assertNotNull(result);
+		assertThat(result.getAccount()).isEqualTo("account");
 
+		verify(memberRepository, times(1)).findById(memberId);
 	}
 
 	@Test
 	@DisplayName("회원이 존재하지 않을 때 DataNotFoundException 발생")
 	void getMemberDetailThrowsExceptionWhenMemberNotFound() {
 		// Given
-		String token = "valid_token";
 		Long memberId = 1L;
 
-		when(jwtManager.getMemberId(token)).thenReturn(memberId);
 		when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
 
 		// When & Then
 		DataNotFoundException exception = assertThrows(DataNotFoundException.class, () ->
-			memberService.getMemberDetail(token)
+			memberService.getMemberDetail(memberId)
 		);
 
-		assertEquals(USER_NOT_FOUND, exception.getStatusCode());
+		verify(memberRepository, times(1)).findById(memberId);
 	}
 }
