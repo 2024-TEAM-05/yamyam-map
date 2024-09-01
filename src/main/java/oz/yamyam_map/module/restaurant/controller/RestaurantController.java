87f55ae8d7d36d5d73647083e48d679b59258e2f
hye-on <ain0103@naver.com>
2024-09-01 10:32:08 +0900

    chore: 네이버 코드 스타일 적용

--- src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
+++ src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
@@ -1,9 +1,7 @@
 package oz.yamyam_map.module.restaurant.controller;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 import oz.yamyam_map.common.BaseApiResponse;
 import oz.yamyam_map.common.code.StatusCode;
 import oz.yamyam_map.module.auth.security.CustomUserDetails;
@@ -25,25 +25,25 @@ import oz.yamyam_map.module.restaurant.service.RestaurantService;
 @RequestMapping("/api/restaurants")
 public class RestaurantController implements RestaurantControllerDocs {
 
-  private final RestaurantService restaurantService;
+	private final RestaurantService restaurantService;
 
-  @ResponseStatus(HttpStatus.OK)
-  @PostMapping("/{restaurantId}/review")
-  public BaseApiResponse<Void> uploadReview(
-      @AuthenticationPrincipal CustomUserDetails userDetails,
-      @PathVariable(name = "restaurantId") Long restaurantId,
-      @RequestBody ReviewUploadReq req) {
+	@ResponseStatus(HttpStatus.OK)
+	@PostMapping("/{restaurantId}/review")
+	public BaseApiResponse<Void> uploadReview(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable(name = "restaurantId") Long restaurantId,
+		@RequestBody ReviewUploadReq req) {
 
-    restaurantService.uploadReview(userDetails.getMemberId(), restaurantId, req);
+		restaurantService.uploadReview(userDetails.getMemberId(), restaurantId, req);
 
-    return BaseApiResponse.of(CREATED);
-  }
+		return BaseApiResponse.of(CREATED);
+	}
 
-  @ResponseStatus(OK)
-  @GetMapping("/{restaurantId}")
-  public BaseApiResponse<RestaurantDetailRes> getRestaurantDetails(
-      @PathVariable(name = "restaurantId") Long restaurantId) {
-    RestaurantDetailRes restaurantDetailRes = restaurantService.getRestaurantDetails(restaurantId);
-    return BaseApiResponse.of(StatusCode.OK, restaurantDetailRes);
-  }
+	@ResponseStatus(OK)
+	@GetMapping("/{restaurantId}")
+	public BaseApiResponse<RestaurantDetailRes> getRestaurantDetails(
+		@PathVariable(name = "restaurantId") Long restaurantId) {
+		RestaurantDetailRes restaurantDetailRes = restaurantService.getRestaurantDetails(restaurantId);
+		return BaseApiResponse.of(StatusCode.OK, restaurantDetailRes);
+	}
 }
