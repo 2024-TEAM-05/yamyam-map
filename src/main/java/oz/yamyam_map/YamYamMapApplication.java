23c4c91f8046dba80cde896c42785e135ece7564
Doeun <id5eun30@gmail.com>
2024-09-01 20:31:53 +0900

    test: RegionControllerTest 작성, 단위테스트를 위해 @EnableJpaAuditing config 파일로 분리 (#27)

--- src/main/java/oz/yamyam_map/YamYamMapApplication.java
+++ src/main/java/oz/yamyam_map/YamYamMapApplication.java
@@ -3,11 +3,9 @@ package oz.yamyam_map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class YamYamMapApplication {
 
