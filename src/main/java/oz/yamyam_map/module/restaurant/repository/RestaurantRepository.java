c1550e22431cfeff8ef8e90d7a2c1296294fe97a
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-09-02 20:18:11 +0900

    feat: Restaurant 을 지번 / 도로명 주소를 통해 찾는 JPA 메서드 추가 (#39)

--- src/main/java/oz/yamyam_map/module/restaurant/repository/RestaurantRepository.java
+++ src/main/java/oz/yamyam_map/module/restaurant/repository/RestaurantRepository.java
@@ -1,8 +1,13 @@
 package oz.yamyam_map.module.restaurant.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import oz.yamyam_map.module.restaurant.entity.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+	Optional<Restaurant> findByNameAndOldAddressFull(String name, String oldAddressFull);
+
+	Optional<Restaurant> findByNameAndRoadAddressFull(String name, String roadAddressFull);
 }
