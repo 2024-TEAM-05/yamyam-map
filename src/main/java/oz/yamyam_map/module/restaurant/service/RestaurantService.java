caf8b6d7d22c963feea8fc500026cab147e3f12f
jooda00 <dragon2000a@gmail.com>
2024-09-03 16:54:28 +0900

    feat: RestaurantService의 getRestaurantDetails에 캐싱 작업 추가 (#38)
    - 리뷰 개수가 10개 이상인 맛집 객체만 캐싱하는 조건 설정

--- src/main/java/oz/yamyam_map/module/restaurant/service/RestaurantService.java
+++ src/main/java/oz/yamyam_map/module/restaurant/service/RestaurantService.java
@@ -1,9 +1,13 @@
 package oz.yamyam_map.module.restaurant.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import oz.yamyam_map.common.code.StatusCode;
 import oz.yamyam_map.exception.custom.BusinessException;
 import oz.yamyam_map.exception.custom.DataNotFoundException;
@@ -19,10 +23,14 @@ import oz.yamyam_map.module.restaurant.repository.ReviewRepository;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class RestaurantService {
+	private static final String CACHE_PREFIX = "restaurant:";
+	private static final int TTL = 24;
 	private final ReviewRepository reviewRepository;
 	private final MemberRepository memberRepository;
 	private final RestaurantRepository restaurantRepository;
+	private final RedisTemplate<String, Object> redisTemplate;
 
 	@Transactional
 	public void uploadReview(Long memberId, Long restaurantId, ReviewUploadReq req) {
@@ -40,8 +48,24 @@ public class RestaurantService {
 	}
 
 	public RestaurantDetailRes getRestaurantDetails(Long restaurantId) {
+		String cacheKey = CACHE_PREFIX + restaurantId;
+		Restaurant cachedRestaurant = (Restaurant)redisTemplate.opsForValue().get(cacheKey);
+
+		if (cachedRestaurant != null) {
+			log.info("{}번 맛집을 캐싱합니다.", restaurantId);
+			return RestaurantDetailRes.from(cachedRestaurant);
+		}
+
+		log.info("캐시에 존재하지 않아 DB에서 조회합니다.");
 		Restaurant restaurant = restaurantRepository.findById(restaurantId)
 			.orElseThrow(() -> new DataNotFoundException(StatusCode.RESTAURANT_NOT_FOUND));
+
+		if (restaurant.getReviewRating().getTotalReviews() >= 10) {
+			log.info("{}번 맛집을 캐시에 저장 작업을 시작합니다.", restaurantId);
+			redisTemplate.opsForValue().set(cacheKey, restaurant, TTL, TimeUnit.HOURS);
+			log.info("{}번 맛집을 캐시에 저장 작업을 완료했습니다.", restaurantId);
+		}
+
 		return RestaurantDetailRes.from(restaurant);
 	}
 }
