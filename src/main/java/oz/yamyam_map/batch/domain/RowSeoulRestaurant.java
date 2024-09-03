7b5efe677e2b7a104730936ca9b21ba9c912059a
uijin-j <uijin0531@gmail.com>
2024-09-04 01:20:50 +0900

    feat: row 엔티티 update() 메서드 추가 및 해싱 처리 로직 추가

--- src/main/java/oz/yamyam_map/batch/domain/RowSeoulRestaurant.java
+++ src/main/java/oz/yamyam_map/batch/domain/RowSeoulRestaurant.java
@@ -11,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import oz.yamyam_map.batch.dto.SeoulRestaurantDto;
+import oz.yamyam_map.batch.util.HashUtils;
 import oz.yamyam_map.common.entity.BaseEntity;
 
 @Entity
@@ -115,7 +116,20 @@ public class RowSeoulRestaurant extends BaseEntity {
 	private String hash; // 변경 감지를 위한 해시값
 
 	public static RowSeoulRestaurant of(SeoulRestaurantDto dto) {
+		return buildFromDto(dto);
+	}
+
+	public RowSeoulRestaurant update(SeoulRestaurantDto dto) {
+		return buildFromDto(this.id, dto);
+	}
+
+	public boolean hasNotChanged(SeoulRestaurantDto newItem) {
+		return hash.equals(calculateHash(newItem));
+	}
+
+	private static RowSeoulRestaurant buildFromDto(Long id, SeoulRestaurantDto dto) {
 		return RowSeoulRestaurant.builder()
+			.id(id)
 			.opnsfteamcode(dto.opnsfteamcode())
 			.mgtno(dto.mgtno())
 			.apvpermymd(dto.apvpermymd())
@@ -160,12 +174,15 @@ public class RowSeoulRestaurant extends BaseEntity {
 			.jtupsoasgnno(dto.jtupsoasgnno())
 			.jtupsomainedf(dto.jtupsomainedf())
 			.homepage(dto.homepage())
-			.hash(calculateHash())
+			.hash(calculateHash(dto))
 			.build();
 	}
 
-	private static String calculateHash() {
-		// TODO: SeoulRestaurantDto의 모든 필드에 대한 해시값 구하기
-		return null;
+	private static RowSeoulRestaurant buildFromDto(SeoulRestaurantDto dto) {
+		return buildFromDto(null, dto);
+	}
+
+	private static String calculateHash(SeoulRestaurantDto record) {
+		return HashUtils.sha256FromObject(record);
 	}
 }
