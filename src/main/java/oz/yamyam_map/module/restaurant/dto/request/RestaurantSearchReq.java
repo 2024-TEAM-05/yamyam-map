eac5159ddb49d4c67a134bc55dae483ad0d57a62
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-03 17:05:38 +0900

    feat: 페이지네이션 관련 응답 상속 및 요청 필드 관련 설명 추가 (#50)

--- src/main/java/oz/yamyam_map/module/restaurant/dto/request/RestaurantSearchReq.java
+++ src/main/java/oz/yamyam_map/module/restaurant/dto/request/RestaurantSearchReq.java
@@ -1,5 +1,6 @@
 package oz.yamyam_map.module.restaurant.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -10,22 +11,24 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
-public class RestaurantSearchReq {
+public class RestaurantSearchReq extends PaginationQuery {
 
+	@Schema(description = "위도")
 	@NotNull(message = "위도는 필수 파라미터입니다.")
 	@Min(value = -90, message = "위도는 -90 이상이어야 합니다.")
 	@Max(value = 90, message = "위도는 90 이하여야 합니다.")
 	private double latitude;
 
+	@Schema(description = "경도")
 	@NotNull(message = "경도는 필수 파라미터입니다.")
 	@Min(value = -180, message = "경도는 -180 이상이어야 합니다.")
 	@Max(value = 180, message = "경도는 180 이하여야 합니다.")
 	private double longitude;
 
+	@Schema(description = "범위")
 	@Min(value = 1, message = "반경은 1m 이상이어야 합니다.")
 	private double range;
-	
-	private String sort;
 
-	// TODO: 페이지네이션 관련 필드 추가
+	@Schema(description = "정렬 방식", allowableValues = {"rating", "distance"}, defaultValue = "rating")
+	private String sort;
 }
