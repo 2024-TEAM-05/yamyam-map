0ab8c2969764ad29894677995abfcd5be30d0404
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-09-03 20:04:01 +0900

    refactor: 각 단계 메소드로 분리 및 중복되는 주소 검증 코드 orElseGet 사용하여 대체 (#39)

--- src/main/java/oz/yamyam_map/batch/processor/SeoulDataProcessor.java
+++ src/main/java/oz/yamyam_map/batch/processor/SeoulDataProcessor.java
@@ -1,6 +1,5 @@
 package oz.yamyam_map.batch.processor;
 
-import static oz.yamyam_map.common.code.StatusCode.*;
 import static oz.yamyam_map.common.util.GeoUtils.*;
 
 import java.util.Objects;
@@ -9,13 +8,14 @@ import org.locationtech.jts.geom.Point;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import oz.yamyam_map.batch.domain.RowSeoulRestaurant;
 import oz.yamyam_map.batch.enums.RestaurantSalesType;
 import oz.yamyam_map.batch.mapper.RestaurantTypeMapper;
 import oz.yamyam_map.batch.util.AddressUtils;
 import oz.yamyam_map.common.enums.RestaurantType;
-import oz.yamyam_map.exception.custom.BusinessException;
 import oz.yamyam_map.module.region.entity.Region;
 import oz.yamyam_map.module.region.repository.RegionRepository;
 import oz.yamyam_map.module.restaurant.entity.Restaurant;
@@ -23,33 +23,24 @@ import oz.yamyam_map.module.restaurant.repository.RestaurantRepository;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SeoulDataProcessor implements ItemProcessor<RowSeoulRestaurant, Restaurant> {
+
+	public static final String SEOUL = "서울";
 
 	private final RestaurantRepository restaurantRepository;
 	private final RegionRepository regionRepository;
 
 	@Override
 	public Restaurant process(RowSeoulRestaurant item) {
-		// 0. 주소 존재 여부 확인
-		// 데이터 확인 시, 도로명 주소가 없다면, 지번 주소는 있고/ 지번 주소가 없다면 도로명 주소는 있었기에 해당 로직으로 처리
-		String address = item.getRdnwhladdr();
+		// 0. 유효한 주소 가져오기
+		String address = getValidAddress(item);
 
 		// 1. 해당 식당명과 지번 주소 혹은 도로병 주소로 값 가져오기
-		Restaurant restaurant;
-		if (address.isEmpty() && address.isBlank()) {
-			address = item.getSitewhladdr();
-			restaurant = restaurantRepository.findByNameAndOldAddressFull(item.getTrdstatenm(), address)
-				.orElse(null);
-		} else {
-			restaurant = restaurantRepository.findByNameAndRoadAddressFull(item.getTrdstatenm(), address)
-				.orElse(null);
-		}
+		Restaurant restaurant = findExistingRestaurant(item.getTrdstatenm(), address);
 
 		// 2. 폐업인 경우 삭제
-		if (Objects.equals(item.getDtlstatenm(), RestaurantSalesType.CLOSED.name())) {
-			if (restaurant != null) {
-				restaurantRepository.delete(restaurant);
-			}
+		if (isRestaurantClosed(item, restaurant)) {
 			return null;
 		}
 
@@ -57,21 +48,50 @@ public class SeoulDataProcessor implements ItemProcessor<RowSeoulRestaurant, Res
 		RestaurantType restaurantType = RestaurantTypeMapper.from(item.getUptaenm());
 
 		// 4. 시군구 매핑
-		Region region = getRegionFromAddress(address);
+		Region region = getRegionByAddress(address);
+		if (region == null) {
+			log.error("SeoulDataProcessor - 해당 시군구를 Region 엔티티에서 찾을 수 없습니다.[ 관리번호: {}, 주소: {} ]", item.getMgtno(),
+				address);
+			return null;
+		}
 
 		// 5. 위, 경도 null 인 경우 시군구 주소에서 위, 경도 가져오기
-		Point point = getXYSafety(item.getX(), item.getY(), region);
+		Point location = getXYSafety(item.getX(), item.getY(), region);
 
 		// 6. 맛집 데이터 반환
-		if (restaurant == null) { // 없었던 데이터인 경우
-			return Restaurant.of(region, item.getTrdstatenm(), restaurantType, item.getSitetel(),
-				point, item.getSitewhladdr(),
-				item.getRdnwhladdr());
-		} else { // 있던 데이터의 수정인 경우
-			return restaurant.updateRestaurant(region, item.getTrdstatenm(), restaurantType, item.getSitetel(),
-				point, item.getSitewhladdr(),
-				item.getRdnwhladdr());
+		return saveOrUpdateRestaurant(restaurant, region, item, restaurantType, location);
+	}
+
+	// 지번/ 도로명 중 있는 주소 가져오기
+	private String getValidAddress(RowSeoulRestaurant item) {
+		return StringUtils.isNotBlank(item.getRdnwhladdr()) ? item.getRdnwhladdr() : item.getSitewhladdr();
+	}
+
+	// 레스토랑이 존재하는지 확인하는 메서드
+	private Restaurant findExistingRestaurant(String name, String address) {
+		if (StringUtils.isBlank(address)) {
+			return null;
 		}
+		return restaurantRepository.findByNameAndOldAddressFull(name, address)
+			.orElseGet(() -> restaurantRepository.findByNameAndRoadAddressFull(name, address).orElse(null));
+	}
+
+	// 폐업 여부 확인하는 메서드
+	private boolean isRestaurantClosed(RowSeoulRestaurant item, Restaurant restaurant) {
+		if (Objects.equals(item.getDtlstatenm(), RestaurantSalesType.CLOSED.name())) {
+			if (restaurant != null) {
+				restaurantRepository.delete(restaurant);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	// 주소에서 서울 / 군구 인스턴스 가져오는 메서드
+	private Region getRegionByAddress(String item) {
+		String cityDistrict = AddressUtils.extractCityDistrict(item);
+		return regionRepository.findByProvinceAndCityDistrict(SEOUL, cityDistrict)
+			.orElse(null);
 	}
 
 	// 위, 경도 없는 경우 시군구로 Point 반환하는 메서드
@@ -83,10 +103,16 @@ public class SeoulDataProcessor implements ItemProcessor<RowSeoulRestaurant, Res
 		return createPoint(Double.parseDouble(x), Double.parseDouble(y));
 	}
 
-	// 주소에서 서울 / 군구 인스턴스 가져오는 메서드
-	private Region getRegionFromAddress(String item) {
-		String cityDistrict = AddressUtils.extractCityDistrict(item);
-		return regionRepository.findByProvinceAndCityDistrict("서울", cityDistrict)
-			.orElseThrow(() -> new BusinessException(REGION_NOT_FOUND_BATCH_SERVER_ERROR));
+	// 수정, 생성 여부에 따라 맛집 저장하는 플로우
+	private Restaurant saveOrUpdateRestaurant(Restaurant restaurant, Region region, RowSeoulRestaurant item,
+		RestaurantType restaurantType, Point location) {
+		if (restaurant == null) {
+			return Restaurant.of(region, item.getTrdstatenm(), restaurantType, item.getSitetel(),
+				location, item.getSitewhladdr(), item.getRdnwhladdr());
+		} else {
+			return restaurant.updateRestaurant(region, item.getTrdstatenm(), restaurantType, item.getSitetel(),
+				location, item.getSitewhladdr(), item.getRdnwhladdr());
+		}
 	}
+
 }
