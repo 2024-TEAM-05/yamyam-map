fa1b8135d9346ae32b396d73db974ce400149daa
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-03 17:09:00 +0900

    feat: 맛집 검색 api 추가 (#50)

--- src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
+++ src/main/java/oz/yamyam_map/module/restaurant/controller/RestaurantController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +17,10 @@ import lombok.RequiredArgsConstructor;
 import oz.yamyam_map.common.BaseApiResponse;
 import oz.yamyam_map.common.code.StatusCode;
 import oz.yamyam_map.module.auth.security.CustomUserDetails;
+import oz.yamyam_map.module.restaurant.dto.request.RestaurantSearchReq;
 import oz.yamyam_map.module.restaurant.dto.request.ReviewUploadReq;
 import oz.yamyam_map.module.restaurant.dto.response.RestaurantDetailRes;
+import oz.yamyam_map.module.restaurant.dto.response.RestaurantListRes;
 import oz.yamyam_map.module.restaurant.service.RestaurantService;
 
 @RestController
@@ -45,5 +48,13 @@ public class RestaurantController implements RestaurantControllerDocs {
 		@PathVariable(name = "restaurantId") Long restaurantId) {
 		RestaurantDetailRes restaurantDetailRes = restaurantService.getRestaurantDetails(restaurantId);
 		return BaseApiResponse.of(StatusCode.OK, restaurantDetailRes);
+	}
+
+	@ResponseStatus(OK)
+	@GetMapping
+	public BaseApiResponse<RestaurantListRes> getRestaurantList(
+		@ModelAttribute RestaurantSearchReq req) {
+		RestaurantListRes restaurantListRes = restaurantService.getRestaurants(req);
+		return BaseApiResponse.of(StatusCode.OK, restaurantListRes);
 	}
 }
