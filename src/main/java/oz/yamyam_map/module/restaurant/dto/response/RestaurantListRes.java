95b8ed16815937a83cef585abf8ef0d021d3c49c
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-03 17:04:34 +0900

    feat: 맛집 목록에 페이징 관련 정보 응답에 추가 (#50)

--- src/main/java/oz/yamyam_map/module/restaurant/dto/response/RestaurantListRes.java
+++ src/main/java/oz/yamyam_map/module/restaurant/dto/response/RestaurantListRes.java
@@ -2,11 +2,19 @@ package oz.yamyam_map.module.restaurant.dto.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class RestaurantListRes {
+	@Schema(description = "전체 맛집 수")
+	private long totalElements;
+
+	@Schema(description = "전체 페이지 수")
+	private int totalPages;
+
+	@Schema(description = "맛집 리스트")
 	private List<RestaurantSearchRes> restaurants;
 }
