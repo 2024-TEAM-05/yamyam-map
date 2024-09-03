27785cb6e867e4016976f693789e5620d71945bc
jooda00 <dragon2000a@gmail.com>
2024-09-03 21:41:45 +0900

    feat: redis config 파일에 objectMapper 메소드 추가 (#38)
    - PointSerializer 직렬화 메소드 등록

--- src/main/java/oz/yamyam_map/core/config/RedisConfig.java
+++ src/main/java/oz/yamyam_map/core/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package oz.yamyam_map.core.config;
 
+import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,11 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import oz.yamyam_map.common.util.GeoUtils;
 
 @Configuration
 public class RedisConfig {
@@ -22,11 +28,20 @@ public class RedisConfig {
 	}
 
 	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		SimpleModule module = new SimpleModule();
+		module.addSerializer(Point.class, new GeoUtils.PointSerializer());
+		mapper.registerModule(module);
+		return mapper;
+	}
+
+	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer()); // 직렬화 작업
-		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
 		return redisTemplate;
 	}
 }
