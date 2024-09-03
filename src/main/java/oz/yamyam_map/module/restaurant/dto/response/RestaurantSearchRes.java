13edd6b88d34b6ab21453972ce1c3da412543083
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-03 17:06:15 +0900

    docs: 맛집 검색 응답 필드 문서화 추가 (#50)

--- src/main/java/oz/yamyam_map/module/restaurant/dto/response/RestaurantSearchRes.java
+++ src/main/java/oz/yamyam_map/module/restaurant/dto/response/RestaurantSearchRes.java
@@ -1,9 +1,8 @@
 package oz.yamyam_map.module.restaurant.dto.response;
 
-import java.util.List;
-
 import org.locationtech.jts.geom.Point;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +12,19 @@ import oz.yamyam_map.common.enums.RestaurantType;
 @Builder
 @AllArgsConstructor
 public class RestaurantSearchRes {
+	@Schema(description = "맛집 id")
 	private Long id;
+
+	@Schema(description = "맛집 상호명")
 	private String name;
-	private RestaurantType businessType;
+
+	@Schema(description = "맛집 종류")
+	private RestaurantType restaurantType;
+
+	@Schema(description = "위도, 경도")
 	private Point location;
+
+	@Schema(description = "맛집 평점")
 	private double averageScore;
 
 }
